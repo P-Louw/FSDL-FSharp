e8af23df66d5b65bed9e4c4971c8138bbc4a322f
Paul <pldbrn.117@gmail.com>
2022-07-25 23:49:18 +0200

    Removed default library file and applied formatting.

--- OkasakiStructures/Chapter2.fs
+++ OkasakiStructures/Chapter2.fs
@@ -15,35 +15,48 @@ type LStack<'a> = {
 module ListStack =
    type ListStack<'a> =
        'a list
+       
    let Empty: ListStack<_> = []
+   
    let isEmpty listStack =
        List.isEmpty listStack
+       
    let cons x xs:ListStack<_> = x::xs
+   
    let head s = List.head s
+   
    let tail s = List.tail s
+   
 /// Figure 2.3 Custom stack with DU 'Empty' for NIL value.
 [<RequireQualifiedAccess>]
 module CustomStack =
     type CStack<'a> =
         | Empty
         | Cons of 'a * CStack<'a>
+        
     let empty = Empty
+    
     let isEmpty = function
         | Empty -> true
         | _ -> false
+        
     let cons x xs = Cons(x, xs)
+    
     let head = function
         | Empty -> failwith "empty"
         | Cons(x, _) -> x
+        
     let tail = function
         | Empty -> failwith "empty"
         | Cons(_, xs) -> xs
+        
     /// Append a CStack list to a existing one in a imperative way.
     /// This 'destroys' the previous list because of mutating the previous representation.
     let rec (++) x xs =
         match x with
         | Empty -> xs
         | Cons(h, t) -> Cons(h, t ++ xs)
+        
     /// Figure 2.4 Append a CStack list to a existing one in a declarative way,
     /// preserving the previous representation by copying from the affected node. 
     let rec update = function
--- OkasakiStructures/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace OkasakiStructures
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
